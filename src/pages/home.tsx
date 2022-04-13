dc787e898f2bb728f6f264ff30cca03c108da377
Ivan <canelo_1987_@hotmail.com>
2022-04-13 17:03:57 +0200

    Fixed Responsive

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,14 +3,11 @@ import "../sass/styles.scss";
 import { Link } from "react-router-dom";
 
 function Home(): JSX.Element {
- 
   return (
     <>
-      <div className="home">
-       
-
+      <div className="home size">
         <img src="/images/rocket.png" alt="rocket logo"></img>
-        
+
         <h1>Prueba tu capacidad con esta Quiz</h1>
         <h2>Responde lo más rapido que puedas y podrás ganar muchos premios</h2>
         <Link to="/question0">
